d27ac71fc5810f49fb645633abc21111b0ab671d
relaxb0y <sergey2002shulga@gmail.com>
2021-12-24 00:22:37 +0200

    final

--- Lab5.py
+++ Lab5.py
@@ -3,17 +3,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
-# driver.implicitly_wait(15)
+driver.implicitly_wait(15)
 
-url = 'https://rp5.ua/'
-search_request = 'Одесса'
+url = 'https://www.citrus.ua'
+search_request = 'IPhone'
 
 driver.get(url)
-# driver.find_element(By.ID, 'searchStr').send_keys(search_request)
-# driver.find_element(By.ID, 'searchStr').send_keys(Keys.ENTER)
-#
-# actualResult = driver.find_element(By.CLASS_NAME, 'searchResults').text
-# expectedResult = search_request
-#
-# assert expectedResult in actualResult
-# driver.close()
+driver.find_element(By.ID, 'searchStr').send_keys(search_request)
+driver.find_element(By.ID, 'searchStr').send_keys(Keys.ENTER)
+
+actualResult = driver.find_element(By.CLASS_NAME, 'searchResults').text
+expectedResult = search_request
+
+assert expectedResult in actualResult
+driver.close()
